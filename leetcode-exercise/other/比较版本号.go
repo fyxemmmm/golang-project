463f337f858e1dc51745d1d269df5fd59b29ec7c
fyxemmmm <979745517@qq.com>
2021-08-26 12:59:45 +0800

    dp

---
+++ leetcode-exercise/other/比较版本号.go
@@ -0,0 +1,29 @@
+package other
+
+import (
+	"strconv"
+	"strings"
+)
+
+func compareVersion(version1 string, version2 string) int {
+	v1 := strings.Split(version1, ".")
+	v2 := strings.Split(version2, ".")
+
+	for len(v1) < len(v2) {
+		v1 = append(v1, "0")
+	}
+	for len(v2) < len(v1) {
+		v2 = append(v2, "0")
+	}
+
+	for i := 0; i < len(v1); i++ {
+		a, _ := strconv.Atoi(v1[i])
+		b, _ := strconv.Atoi(v2[i])
+		if a > b {
+			return 1
+		} else if a < b {
+			return -1
+		}
+	}
+	return 0
+}
